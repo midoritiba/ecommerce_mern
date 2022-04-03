e067e892c815e292b67ccabc54a28e17a2176d1b
midoritiba <mic.tiba@gmail.com>
2022-04-03 18:58:44 +0200

    Categories Component added

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
+import Categories from '../components/Categories'
 // import { Row, Col } from 'react-bootstrap';
 
 const HomePage = () => {
   return (
     <>
      <HeroSection/>
+     <Categories/>
     </>
   )
 }
